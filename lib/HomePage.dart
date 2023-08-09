b2b041c2ec7cf0f34f00a218ba007f45a506cfb3
aayushpatel006 <rudaniaayush1@gmail.com>
2023-08-09 14:57:06 +0530

    inital state and reset method done

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,8 +9,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // TODO import image
+  AssetImage circle = AssetImage('images/cirlce.png')
+  AssetImage lucky = AssetImage('images/rupee.png')
+  AssetImage unlucky = AssetImage('images/sadFace.png')
+
+  // TODO get an array
+  List<String> itemArray = List<String>.generate(25, (index) => "empty");
+  int? luckyNumber;
+
+  // TODO init arrray with 23 elements
+  @override
+  void initState() {
+    super.initState();
+    itemArray = List<String>.generate(25, (index) => "empty");
+    genetateRandomNumber();
+  }
+  genetateRandomNumber() {
+    setState(() {
+      luckyNumber = Random().nextInt(25);
+    });
+  }
+  
+
+  // TODO define getImage method
+
+  // TODO play game method
+
+  // TODO showall
+
+  // TODO reset all
+  resetGame() {
+    setState(() {
+      itemArray = List<String>.filled(25, "empty");
+      genetateRandomNumber();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scratch Application"),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Scratch Application",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.pink),
       home: HomePage(),
     );
   }
